4e6b70fe78472d5e2eb75f3d456d3f1c98e6f9a0
unknown <53538316+grlucy@users.noreply.github.com>
2020-01-10 09:57:23 -0500

    created dynamic-html.js

---
+++ assets/javascript/dynamic-html.js
@@ -0,0 +1,11 @@
+const landingContent = `
+<button class="navBtn" id="aboutLanding">About</button>
+<button class="navBtn" id="projectsLanding">Projects</button>
+<button class="navBtn" id="contactLanding">Contact</button>
+`;
+
+const aboutContent = ``;
+
+const projectsContent = ``;
+
+const contactContent = ``;
