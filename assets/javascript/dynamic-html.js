f0a0f4228880242b35c1f1015b7d84f03de1ff52
unknown <53538316+grlucy@users.noreply.github.com>
2020-01-10 11:13:11 -0500

    added about content

--- assets/javascript/dynamic-html.js
+++ assets/javascript/dynamic-html.js
@@ -4,7 +4,51 @@ const landingContent = `
 <button class="navBtn" id="contactLanding">Contact</button>
 `;
 
-const aboutContent = ``;
+const aboutContent = `
+<div class="row">
+ <div class="col-md-3 about-col">
+   <img
+     src="./assets/images/GINALUCY.jpg"
+     alt="Gina Lucy Photo"
+     id="headshot"
+   />
+ </div>
+ <div class="col-md-9 about-col">
+   <p class="d-flex flex-wrap variable-justify">
+     <span class="about-details">(757) 652-3703</span>
+     <span class="about-details"
+       ><a href="mailto:ginalucy9@gmail.com"
+         >ginalucy9@gmail.com</a
+       ></span
+     >
+     <span class="about-details"
+       ><a href="#" target="_blank">Download my resume</a></span
+     >
+   </p>
+   <p>
+     I am currently a student at the University of Richmond
+     studying full stack web development. Before returning to
+     university, I was a Senior Communications Specialist at
+     CBRE, a global commercial real estate leader. I worked in
+     the marketing field from 2011 to 2019 at companies including
+     CBRE, Virginia Beach Community Development Corporation, and
+     Old Dominion University. I also managed my own freelance
+     business, providing services in photography, graphic design,
+     and web design.
+   </p>
+   <p>
+     Raised in Virginia Beach, I lived in Williamsburg, Norfolk,
+     and Baltimore before moving to Richmond in spring 2018. I
+     graduated with a B.S. Business Administration with a major
+     in marketing from Old Dominion University in 2012.
+   </p>
+   <p>
+     When I'm not coding, I'm playing video games, dancing,
+     traveling, or lazing around with my two cats.
+   </p>
+ </div>
+</div>
+`;
 
 const projectsContent = ``;
 
